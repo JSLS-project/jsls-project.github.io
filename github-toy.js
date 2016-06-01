29ff1dcf4590f06d1f1a219c0077d02e7cca3f1f
Nicholas Jackson <smartguy1196@gmail.com>
2016-06-01 10:31:55 -0700

    Update github-toy.js

--- github-toy.js
+++ github-toy.js
@@ -37,7 +37,7 @@ window.onload = function(){
         commit_doc.appendChild(document.createElement("br"));
         commit_doc.appendChild(commit_link);
         status_doc.appendChild(commit_doc);
-        status.appendChild(status_doc);
+        status.innerHTML = status_doc;
       })
     },
     "repos" : function(){
