03e8f37a0a0ae6b8ef91932ca09094d6056bb4e6
Nicholas Jackson <smartguy1196@gmail.com>
2016-06-01 08:48:45 -0700

    Update github-toy.js

--- github-toy.js
+++ github-toy.js
@@ -1,17 +1,19 @@
-var gittoy = {
-  "toy" : document.getElementById("github-buttons"),
-  "door" : document.getElementById("github-buttons").getAttribute("data-door"),
-  "view" : document.getElementById("github-buttons").getAttribute("data-github"),
-  "useDoor" : function(){
-    if(this.door == "closed"){
-      this.door = "opened";
-    }else{
-      this.door = "closed";
-    }
-    this.toy.setAttribute("data-door",this.door);
-  },
-  "setView" : function(view){},
-  "pullStatus" : function(){},
-  "pullRepos" : function(){},
-  "pullData" : function(){}
+window.onload = function(){
+  var gittoy = {
+    "toy" : document.getElementById("github-buttons"),
+    "door" : document.getElementById("github-buttons").getAttribute("data-door"),
+    "view" : document.getElementById("github-buttons").getAttribute("data-github"),
+    "useDoor" : function(){
+      if(this.door == "closed"){
+        this.door = "opened";
+      }else{
+        this.door = "closed";
+      }
+      this.toy.setAttribute("data-door",this.door);
+    },
+    "setView" : function(view){},
+    "pullStatus" : function(){},
+    "pullRepos" : function(){},
+    "pullData" : function(){}
+  }
 }
