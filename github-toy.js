8f82a5e7a11d9248c39bc82614c86be45c49ff76
Nicholas Jackson <smartguy1196@gmail.com>
2016-06-01 09:59:29 -0700

    Update github-toy.js

--- github-toy.js
+++ github-toy.js
@@ -26,13 +26,13 @@ window.onload = function(){
             commit_doc = document.createElement("div"),
             commit_text = document.createElement("p"),
             commit_link = document.createElement("a");
-        user_link.onclick = a(response.committer.html_url);
+        user_link.onclick = "a(response.committer.html_url)";
         user_link.innerText = response.commit.author.name + " (" + response.committer.login + ")"
         user_doc.appendChild(user_link);
         status_doc.appendChild(user_doc);
         commit_text.innerText = response.commit.message;
         commit_doc.appendChild(commit_text);
-        commit_link.onclick = a(response.html_url);
+        commit_link.onclick = "a(response.html_url)";
         commit_link.innerText = "commit: " + response.sha.substring(0,7);
         commit_doc.appendChild(commit_link);
         status_doc.appendChild(commit_doc);
