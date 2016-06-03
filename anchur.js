eb81d2300971155ac136e4075890ddf7e2f19215
Nicholas Jackson <smartguy1196@gmail.com>
2016-06-03 11:59:35 -0700

    Update anchur.js

--- anchur.js
+++ anchur.js
@@ -48,7 +48,7 @@ var anchur = {
       },
       "contributors" : function(){
         console.log(typeof window.anchur.renav);
-        window.anchur.renav(["contributors"]).call();
+        window.anchur.renav.call(window.anchur,["contributors"]);
       },
       "repos" : function(){
         window.anchur.renav(["repos"]);
