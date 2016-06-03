663f4a0f43f8abafd78d45f5af41357e868cdf97
Nicholas Jackson <smartguy1196@gmail.com>
2016-06-03 06:37:25 -0700

    Update github-toy.js

--- github-toy.js
+++ github-toy.js
@@ -17,29 +17,6 @@ window.onload = function(){
       eval("window.gittoy." + view + "()");
     },
     "status" : function(){
-      pull_data("https://api.github.com/repos/JSLS-project/JavaScript-Language-Syntax-Schema/commits/master", function(response){
-        response = JSON.parse(response)
-        var status = document.getElementById("github-status"),
-            status_doc = document.createElement("div"),
-            user_doc = document.createElement("div"),
-            user_link = document.createElement("a"),
-            commit_doc = document.createElement("div"),
-            commit_text = document.createElement("p"),
-            commit_link = document.createElement("a");
-        user_link.setAttribute("onclick", "a('" + response.committer.html_url + "')");
-        user_link.innerText = response.commit.author.name + " (" + response.committer.login + ")"
-        user_doc.appendChild(user_link);
-        status_doc.appendChild(user_doc);
-        commit_text.innerText = '"' + response.commit.message + '"';
-        commit_doc.appendChild(commit_text);
-        commit_link.setAttribute("onclick", "a('" + response.html_url + "')");
-        commit_link.innerText = "commit: " + response.sha.substring(0,7);
-        commit_doc.appendChild(document.createElement("br"));
-        commit_doc.appendChild(commit_link);
-        status_doc.appendChild(commit_doc);
-        status.innerHTML = "";
-        status.appendChild(status_doc);
-      })
     },
     "repos" : function(){
     },
